68776d72dc463a01d0e63e2915b8aedc48a59092
KiatLun <122871374+KiatLun@users.noreply.github.com>
2026-08-27 15:35:46 +0800

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,69 +1,16 @@
-import { useState } from "react";
-import { 
-  testLLM,
-  getASRModels,
-} from "./services/api";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import type { HuggingFaceModel } from "./types/model";
+import DashboardPage from './pages/DashboardPage';
+
 
 function App() {
-  const [response, setResponse] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [models, setModels] = useState<HuggingFaceModel[]>([]);
-  const [modelsLoading, setModelsLoading] = useState<boolean>(false);
-
-  async function handleTestLLM() {
-    try {
-      setLoading(true);
-
-      const data = await testLLM();
-
-      setResponse(data.response);
-    } catch (error) {
-      console.error(error);
-      setResponse("Something went wrong");
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function handleGetASRModels() {
-    try {
-      setModelsLoading(true);
-
-      const data = await getASRModels();
-      console.log("Fetched ASR models:", data.models);
-      setModels(data.models);
-    } catch (error) {
-      console.error(error);
-      setResponse("Something went wrong");
-    } finally {
-      setModelsLoading(false);
-    }
-  }
-
   return (
-    <div>
-      <h1>ASR Research Dashboard</h1>
-
-      <button onClick={handleTestLLM}>
-        {loading ? "Loading..." : "Test LLM"}
-      </button>
-
-      <p>{response}</p>
-
-      <button onClick={handleGetASRModels}>
-        {modelsLoading ? "Loading..." : "Get ASR Models"}
-      </button>
-      
-      <ul>
-        {models.map((model) => (
-          <li key={model.url}>
-            {model.title}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
