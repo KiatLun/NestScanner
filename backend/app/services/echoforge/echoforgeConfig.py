8e958e4d0b77066709745ac47e9d0dfb6d07f671
AaronJoelTan <109526996+AaronJoelTan@users.noreply.github.com>
2026-09-16 14:58:31 +0800

    Remove duplicate echoforge_root in echoforgeConfig

--- backend/app/services/echoforge/echoforgeConfig.py
+++ backend/app/services/echoforge/echoforgeConfig.py
@@ -1,12 +1,7 @@
 from pathlib import Path
 import os
 
-ECHOFORGE_ROOT = Path(
-    os.getenv(
-        "ECHOFORGE_ROOT",
-        "/mnt/c/Users/kiatlun/Documents/GitHub/EchoForge",
-    )
-)
+from dotenv import load_dotenv
 
 # ----------------------------------------
 # NestScanner root
@@ -68,6 +63,37 @@ MODEL_UPLOAD_DIR = DEPLOYMENT_DIR / "models_upload"
 
 CACHE_DIR = DEPLOYMENT_DIR / ".cache"
 
+
+# ----------------------------------------
+# echoforge component paths
+# ----------------------------------------
+
+COMPONENTS_DIR = ECHOFORGE_ROOT / "components"
+
+INFERENCE_COMPONENT_DIR = COMPONENTS_DIR / "inference_component"
+
+STT_INFERENCE_DIR = INFERENCE_COMPONENT_DIR / "stt_inference"
+
+EVALUATION_COMPONENT_DIR = COMPONENTS_DIR / "evaluation_component"
+
+STT_EVALUATION_DIR = EVALUATION_COMPONENT_DIR / "stt_evaluation"
+
+STT_EVALUATION_FILE = STT_EVALUATION_DIR / "main.py"
+
+
+# ----------------------------------------
+# echoforge pipeline paths
+# ----------------------------------------
+
+PIPELINE_DIR = COMPONENTS_DIR / "pipeline"
+
+PIPELINE_SRC_DIR = PIPELINE_DIR / "src"
+
+PIPELINE_CONF_DIR = PIPELINE_SRC_DIR / "conf"
+
+PIPELINE_MAIN_FILE = PIPELINE_SRC_DIR / "main.py"
+
+NESTSCANNER_PIPELINE_CONF_DIR = PIPELINE_CONF_DIR / "nestscanner"
 
 # ----------------------------------------
 # echoforge environment paths
